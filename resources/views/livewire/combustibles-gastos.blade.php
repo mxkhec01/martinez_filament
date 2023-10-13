d7ea8e79574421e781464911157eca9f780ca220
Hugo Contreras <huguin@hotmail.com>
2023-10-13 09:35:24 -0600

    combustible puesto - corregido

--- resources/views/livewire/combustibles-gastos.blade.php
+++ resources/views/livewire/combustibles-gastos.blade.php
@@ -2,7 +2,7 @@
     @if($viajeGasto > 0)
         <x-filament::section collapsible>
             <x-slot name="heading" class="bg-blue-300">
-                Combustibles {{ array_sum($viajeGasto) }}  Efectivo: {{ $viajeGasto[0] }}  Convenio: {{ $viajeGasto[1] }}
+                Combustibles {{ array_sum($viajeGasto) }}  Efectivo: {{ isset($viajeGasto[0]) ? $viajeGasto[0] : 0 }}  Convenio: {{ isset($viajeGasto[1]) ? $viajeGasto[1] : 0 }}
             </x-slot>
         <div class="mb-4">
             {{ $this->table }}
