bc4992436ea8b19f96c14c2eebfa34b70803d7dd
Hugo Contreras <huguin@hotmail.com>
2023-12-06 11:03:19 -0600

    combustible puesto - corregido

--- resources/views/tables/columns/imagenes-caseta.blade.php
+++ resources/views/tables/columns/imagenes-caseta.blade.php
@@ -1,5 +1,5 @@
 <div class="flex -space-x-2">
-    @if(file_exists(asset('storage' . $getRecord()->foto_url))&& filesize(asset('storage' . $getRecord()->foto_url) >0 ))
+    @if(file_exists(asset('storage' . $getRecord()->foto_url))&& filesize(asset('storage' . $getRecord()->foto_url))>0)
     <a href="{{ asset('storage' . $getRecord()->foto_url) }}" target="_blank"><img
             src="{{ asset('storage' . $getRecord()->foto_url) }}"
             style="height: 2rem; width: 2rem;"
@@ -7,6 +7,5 @@
     </a>
     @else
         <img src="{{ asset('storage' . 'default.jpg') }}" style="height: 2rem; width: 2rem;" class="max-w-none object-cover object-center rounded-full ring-white dark:ring-gray-900 ring-2">
-
     @endif
 </div>
