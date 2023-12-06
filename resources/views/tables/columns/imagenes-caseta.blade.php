64f132cf8c2aaf3dec74116381785f6e0be45e8c
Hugo Contreras <huguin@hotmail.com>
2023-12-05 21:36:32 -0600

    combustible puesto - corregido

--- resources/views/tables/columns/imagenes-caseta.blade.php
+++ resources/views/tables/columns/imagenes-caseta.blade.php
@@ -2,6 +2,7 @@
     <a href="{{ asset('storage' . $getRecord()->foto_url) }}" target="_blank"><img
             src="{{ asset('storage' . $getRecord()->foto_url) }}"
             style="height: 2rem; width: 2rem;"
+            alt="no hay"
             class="max-w-none object-cover object-center rounded-full ring-white dark:ring-gray-900 ring-2">
     </a>
 </div>
